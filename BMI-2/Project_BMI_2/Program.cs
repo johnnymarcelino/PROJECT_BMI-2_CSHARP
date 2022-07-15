1a21fb87f749e4584fde98a02f9e8f7db4d60789
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-07-15 16:35:27 -0300

    Project and solution added

---
+++ BMI-2/Project_BMI_2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Project_BMI_2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
