befacb4391c1a6136ac7555c3673a71124f5b4d7
Johnny Marcelino <100697981+johnnymarcelino@users.noreply.github.com>
2022-07-15 16:48:02 -0300

    Delete BMI-2 directory

--- BMI-2/Project_BMI_2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Project_BMI_2 {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
